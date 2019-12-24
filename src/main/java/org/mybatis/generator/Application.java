31bc3e897739bf03dd3d0262c41c0ecd8ea66393
dengyh2018 <543645284@qq.com>
2019-12-24 17:22:20 +0800

    启动项目

--- src/main/java/org/mybatis/generator/Application.java
+++ src/main/java/org/mybatis/generator/Application.java
@@ -3,46 +3,23 @@ package org.mybatis.generator;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 //@EnableCaching
-@ComponentScan({"org.mybatis"})
+//@EnableDubboConfiguration
+//@ImportResource(locations = "classpath:dubbo.xml")
 @MapperScan("org.mybatis.generator.modules.mapper")
-
-public class Application {
-
-    private final static String DATASOURCE_PREIFX = "spring.datasource";
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    /*@Bean
-    @ConfigurationProperties(prefix = DATASOURCE_PREIFX)
-    public DataSource dataSource() {
-        return new org.apache.tomcat.jdbc.pool.DataSource();
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 
-    @Bean
-    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
-
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSource());
-
-        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-
-        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mapper/*.xml"));
-
-        return sqlSessionFactoryBean.getObject();
-    }
-
-
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource());
-    }
-*/
 }
