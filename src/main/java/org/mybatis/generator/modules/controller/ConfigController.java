31804ff5163968c63bce48c21dfd5a9453154625
dengyh <dengyihong@ad61v1.com>
2021-01-27 10:14:28 +0800

    test-27-1-合并

--- src/main/java/org/mybatis/generator/modules/controller/ConfigController.java
+++ src/main/java/org/mybatis/generator/modules/controller/ConfigController.java
@@ -126,7 +126,7 @@ public class ConfigController {
             return R.ok(); //23232323   //asadadasd   //asadadasd
         } catch (Exception e) {//dyh-master
             e.printStackTrace();//asfdasddasd
-            return R.error(e.getMessage());  //dsadadasasdasad    ///asdadasdas   、、阿达阿达的
+            return R.error(e.getMessage());  //dsadadasasdasad    ///asdadasdas   //asdasdsa   //asdasdasdasd
         }
     }
 
